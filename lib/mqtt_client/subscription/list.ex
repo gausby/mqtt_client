42855d3df7c2f7ad554f0a6ddb2c34d6a74b48df
Martin Gausby <martin@gausby.dk>
2017-06-03 16:55:02 +0200

    Refactored the dispatching algorithm
    Doesn't build nested lists; it might still repeat subscriptions,
    depending on how a process subscribes to topics (i.e. both subscribing
    to "a/#" and "a/b/c" would result in the process being registered
    twice) but this is handled by dropping the result list into a
    MapSet. This could perhaps be optimized by properly handling of
    subscriptions, so a process would only be represented once for topic
    subscriptions that overshadows.

--- lib/mqtt_client/subscription/list.ex
+++ lib/mqtt_client/subscription/list.ex
@@ -29,32 +29,17 @@ defmodule MqttClient.Subscription.List do
     end
   end
 
-  defp catch_all(pid, {:root, 0} = key) do
-    case :ets.lookup(__MODULE__, key) do
-      [{^key, subs, nil}] ->
-        :ets.insert(__MODULE__, {key, subs, MapSet.new([pid])})
-
-      [{^key, subs, catch_alls}] ->
-        :ets.insert(__MODULE__, {key, subs, MapSet.put(catch_alls, pid)})
-    end
-  end
   defp catch_all(pid, {{topic, level}, parent} = key) do
     case :ets.lookup(__MODULE__, key) do
-      [{{{^topic, ^level}, ^parent}, subs, nil}] ->
-        :ets.insert(__MODULE__, {key, subs, MapSet.new([pid])})
-
-      [{{{^topic, ^level}, ^parent}, subs, catch_alls}] ->
-        :ets.insert(__MODULE__, {key, subs, MapSet.put(catch_alls, pid)})
+      [{{{^topic, ^level}, ^parent}, _subs, catch_alls}] ->
+        :ets.update_element(__MODULE__, key, {3, [pid | catch_alls]})
     end
   end
 
   defp subscribe(pid, {{topic, level}, parent} = key) do
     case :ets.lookup(__MODULE__, key) do
-      [{{{^topic, ^level}, ^parent}, nil, catch_alls}] ->
-        :ets.insert(__MODULE__, {key, MapSet.new([pid]), catch_alls})
-
-      [{{{^topic, ^level}, ^parent}, subscriptions, catch_alls}] ->
-        :ets.insert(__MODULE__, {key, MapSet.put(subscriptions, pid), catch_alls})
+      [{{{^topic, ^level}, ^parent}, subscriptions, _catch_alls}] ->
+        :ets.update_element(__MODULE__, key, {2, [pid | subscriptions]})
     end
   end
 
@@ -64,71 +49,74 @@ defmodule MqttClient.Subscription.List do
     do: {{topic, level}, parent}
 
 
-  defp insert_edges([]), do: insert_edge({{:root, 0}, nil})
-  defp insert_edges([{_, 1} = edge]), do: insert_edge(edge, {:root, 0})
+  defp insert_edges([]),
+    do: insert_edge({{:root, 0}, nil})
+  defp insert_edges([{_, 1} = edge]),
+    do: insert_edge({edge, :root})
   defp insert_edges([edge, from|rest]) do
-    insert_edge(edge, from)
+    {parent, _} = from
+    insert_edge({edge, parent})
     insert_edges([from|rest])
   end
 
-  defp insert_edge({{:root, 0}, nil}) do
-    :ets.insert_new(__MODULE__, {{{:root, 0}, nil}, nil, nil})
-  end
-  defp insert_edge({topic, level}, {from, _}) do
-    :ets.insert_new(__MODULE__, {{{topic, level}, from}, nil, nil})
+  defp insert_edge({{_topic, _level}, _from} = key) do
+    :ets.insert_new(__MODULE__, {key, [], []})
   end
 
-  def get_node(topic, level, parent) do
-    :ets.lookup(__MODULE__, {{topic, level}, parent})
-  end
 
   def lookup(topics) do
     {:ok, query} = Topic.valid?(topics)
     query
     |> Enum.with_index(1)
     |> collect(:root)
-    |> List.flatten()
-    |> Enum.reduce(get_root(), fn(item, acc) -> MapSet.union(acc, item) end)
+    |> Enum.into(MapSet.new(get_root()))
+  end
+
+  def get_node(topic, level, parent) do
+    :ets.lookup(__MODULE__, {{topic, level}, parent})
   end
 
   defp get_root() do
-    case :ets.lookup(__MODULE__, {{:root, 0}, nil}) do
-      [] -> MapSet.new()
-      [{{{:root, 0}, nil}, nil, nil}] -> MapSet.new()
-      [{{{:root, 0}, nil}, nil, catch_alls}] -> catch_alls
+    case get_node(:root, 0, nil) do
+      [{{{:root, 0}, nil}, _, catch_alls}] -> catch_alls
     end
   end
 
   defp collect([], _), do: []
   defp collect([{topic, level}], parent) do
-    for current_topic <- ["+", topic] do
-      case get_node(current_topic, level, parent) do
-        [{{{^current_topic, ^level}, ^parent}, nil, catch_alls}] ->
-          catch_alls
+    topic_level =
+      case get_node(topic, level, parent) do
+        [{{{^topic, ^level}, ^parent}, subs, catch_alls}] ->
+          subs ++ catch_alls
 
-        [{{{^current_topic, ^level}, ^parent}, subs, nil}] ->
-          subs
-
-        [{{{^current_topic, ^level}, ^parent}, subs, catch_alls}] ->
-          MapSet.union(subs, catch_alls)
-
-        [] -> nil
+        [] -> []
       end
-    end
-    |> Enum.reject(fn(a) -> a == nil end)
+    wildcard_level =
+      case get_node("+", level, parent) do
+        [{{{"+", ^level}, ^parent}, subs, catch_alls}] ->
+          subs ++ catch_alls
+
+        [] -> []
+      end
+
+    topic_level ++ wildcard_level
   end
   defp collect([{topic, level}|rest], parent) do
-    for current_topic <- ["+", topic] do
-      case get_node(current_topic, level, parent) do
-        [{{{^current_topic, ^level}, ^parent}, _, nil}] ->
-          collect(rest, current_topic)
+    topic_level =
+      case get_node(topic, level, parent) do
+        [{{{^topic, ^level}, ^parent}, _, catch_alls}] ->
+          catch_alls ++ collect(rest, topic)
 
-        [{{{^current_topic, ^level}, ^parent}, _, catch_alls}] ->
-          [catch_alls] ++ collect(rest, current_topic)
-
-        [] -> nil
+        [] -> []
       end
-    end
-    |> Enum.reject(fn(a) -> a == nil end)
+    wildcard_level =
+      case get_node("+", level, parent) do
+        [{{{"+", ^level}, ^parent}, _, catch_alls}] ->
+          catch_alls ++ collect(rest, "+")
+
+        [] -> []
+      end
+
+    topic_level ++ wildcard_level
   end
 end
--- test/mqtt_client/subscription_test.exs
+++ test/mqtt_client/subscription_test.exs
@@ -10,20 +10,20 @@ defmodule MqttClient.SubscriptionTest do
     {:ok, context}
   end
 
-  test "hello", context do
-    IO.inspect context[:subscription_pid]
-  end
-
   test "stuff", context do
-    # Subscription.List.insert(self(), "a/b/+/c")
-    Subscription.List.insert(context[:subscription_pid], "a/c/hest/#")
+    Subscription.List.insert(self(), "a/b/+/c")
+    Subscription.List.insert(context[:subscription_pid], "a/c/hest")
+    Subscription.List.insert(context[:subscription_pid], "+/+/hest")
+    Subscription.List.insert(self(), "a/c/+")
+    Subscription.List.insert(self(), "+/#")
     Subscription.List.insert(self(), "#")
 
-    for _ <- 1..50000 do
-      Subscription.List.lookup("a/b/hest")
-    end
-    IO.inspect Subscription.List.lookup("a/c/hest")
+    # for _ <- 1..500_000 do
+    #   Subscription.List.lookup("a/b/hest")
+    # end
+    # IO.inspect Subscription.List.lookup("a/c/hest")
+    # IO.inspect Subscription.List.lookup("hest/a")
 
-    IO.inspect "done"
+    # IO.inspect "done"
   end
 end
